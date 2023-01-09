4d132af73bddbf2b89a2e068b0acbc0d5915969e
melsayed-TA <114928829+melsayed-TA@users.noreply.github.com>
2023-01-09 23:40:57 +0100

    Update MVBuchungAnnonymUsingExcel.java

--- src/test/java/tests/MVBuchungAnnonymUsingExcel.java
+++ src/test/java/tests/MVBuchungAnnonymUsingExcel.java
@@ -18,7 +18,9 @@ public class MVBuchungAnnonymUsingExcel extends TestBase {
 	@DataProvider(name="MVTestData")
 	public static String[][] getTestData() throws IOException
 	{
-		String excelFilePath	= System.getProperty("user.dir") + "\\src\\test\\resources\\testDataFiles\\MV_TestData.xlsx";
+		//String excelFilePath	= System.getProperty("user.dir") + "\\src\\test\\resources\\testDataFiles\\MV_TestData.xlsx";
+		String excelFilePath	= "\\src\\test\\resources\\testDataFiles\\MV_TestData.xlsx";
+
 		String sheetName		= "MV_Testdaten_Iterations";
 		ExcelFileDataReader objExcel = new ExcelFileDataReader(excelFilePath, sheetName);
 		String[][] excelData = objExcel.getExcelData();
