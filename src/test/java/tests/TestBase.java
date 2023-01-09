ac3ddecf55d3277c79761108ec316a7cc82352da
melsayed-TA <114928829+melsayed-TA@users.noreply.github.com>
2023-01-10 00:15:51 +0100

    Update TD Files paths

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -23,7 +23,7 @@ public class TestBase {
 	private String umgebung 					= PropertiesFileDataReader.userData.getProperty("umgebung");	//Read Umgebung from properties file (Best way to store it!). Ex: EK1
 	
 	//private String excelPath_SystemsAndUsers	= System.getProperty("user.dir") + "\\src\\test\\resources\\testDataFiles\\SystemsAndUsers.xlsx";
-	private String excelPath_SystemsAndUsers	= "./src\\test\\resources\\testDataFiles\\SystemsAndUsers.xlsx";
+	private String excelPath_SystemsAndUsers	= "src\\test\\resources\\testDataFiles\\SystemsAndUsers.xlsx";
 	
 	private String sheetName_Systems			= "Systems_" + umgebung;	//Systems_EK1
 	private ExcelFileDataReader excelSystems;
@@ -34,7 +34,7 @@ public class TestBase {
 	private ExcelFileDataReader excelUsers;				
 	
 	//private String excelPath_Testdaten 			= System.getProperty("user.dir") + "\\src\\test\\resources\\testDataFiles\\MV_TestData_Different_Tests.xlsx";
-	private String excelPath_Testdaten 			= "./src\\test\\resources\\testDataFiles\\MV_TestData_Different_Tests.xlsx";
+	private String excelPath_Testdaten 			= "src\\test\\resources\\testDataFiles\\MV_TestData_Different_Tests.xlsx";
 	
 	private String sheetName_Testdaten			= "MV_Testdaten"; 														
 	private ExcelFileDataReader excelTestDaten;			
